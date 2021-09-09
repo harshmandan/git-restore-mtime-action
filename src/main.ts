0b598fa6c878c69b23990296c3ab0a7925c97b9d
Harsh Mandan <harshsinghmandan@gmail.com>
2021-09-09 12:16:45 +0530

    Removed core

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import * as exec from "@actions/exec";
 import * as path from "path";
 
 async function run() {
-	core.startGroup("Running mtime restore script");
+	// core.startGroup("Running mtime restore script");
 	const script = path.normalize(path.join(__dirname, "..", "src", "git-restore-mtime"));
 	await exec.exec("python", [script]);
-	core.endGroup();
+	// core.endGroup();
 }
 
 run();
